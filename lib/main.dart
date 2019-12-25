8e0dd94c5bba43261d6848156e4117f0d4244d11
Atul Kumar <atul@pocketpills.com>
2019-12-25 16:42:27 +0530

    feat: add project screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Marvel Chat',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.red,
